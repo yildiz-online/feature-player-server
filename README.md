174a8b359aaca2139ac9531c09e20732f3e4c96e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-11-29 06:49:06 +0100

    [YFPSV-0] Initial commit.

---
+++ README.md
@@ -0,0 +1,58 @@
+# Yildiz-Engine feature-player-shared
+
+This is the official repository of The feature-player-server library, part of the Yildiz-Engine project.
+The library contains all the server components to use the player feature.
+
+## Features
+
+* Player persistence.
+* ...
+
+## Requirements
+
+To build this module, you will need a java 8 JDK and Maven 3.
+
+## Coding Style and other information
+
+Project website:
+http://www.yildiz-games.be
+
+Issue tracker:
+https://yildiz.atlassian.net/browse/YFPLSV
+
+Wiki:
+https://yildiz.atlassian.net/wiki
+
+Quality report:
+https://sonarcloud.io/dashboard?id=be.yildiz-games%3Afeature-player-server
+
+## License
+
+All source code files are licensed under the permissive MIT license
+(http://opensource.org/licenses/MIT) unless marked differently in a particular folder/file.
+
+## Build instructions
+
+Go to your root directory, where you POM file is located.
+
+Then invoke maven
+
+	mvn clean install
+
+This will compile the source code, then run the unit tests, and finally build a jar file.
+
+## Usage
+
+In your maven project, add the dependency
+
+```xml
+<dependency>
+    <groupId>be.yildiz-games</groupId>
+    <artifactId>feature-player-server</artifactId>
+    <version>LATEST</version>
+</dependency>
+```
+Replace LATEST with the correct version.
+
+## Contact
+Owner of this repository: Grégory Van den Borre
---
+++ src/main/java/be/yildizgames/engine/feature/player/persistence/PersistentPlayer.java
@@ -0,0 +1,141 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2017 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+
+package be.yildizgames.engine.feature.player.persistence;
+
+import be.yildiz.common.collections.Sets;
+import be.yildiz.common.id.PlayerId;
+import be.yildiz.module.database.data.PersistentData;
+import be.yildizgames.engine.feature.player.Player;
+import be.yildizgames.engine.feature.player.PlayerManager;
+import be.yildizgames.engine.feature.player.PlayerRight;
+import be.yildizgames.engine.feature.player.PlayerToCreate;
+import be.yildizgames.engine.feature.player.generated.database.tables.Players;
+import be.yildizgames.engine.feature.player.generated.database.tables.records.PlayersRecord;
+import org.jooq.DSLContext;
+import org.jooq.conf.Settings;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Persistent data for player.
+ *
+ * @author Grégory Van den Borre
+ */
+public final class PersistentPlayer implements PersistentData<PlayerToCreate, Player> {
+
+    /**
+     * Persistent unit name where data must be retrieved.
+     */
+    private static final Players table = Players.PLAYERS;
+
+    /**
+     * List of unused player's id.
+     */
+    private final Set<PlayerId> freeId;
+
+    /**
+     * Manager for players.
+     */
+    private final PlayerManager playerManager;
+
+    /**
+     * Full constructor, retrieve data from persistent context.
+     *
+     * @param c SQL connection.
+     * @param playerManager Player manager.
+     */
+    public PersistentPlayer(final Connection c, final PlayerManager playerManager) {
+        super();
+        this.freeId = Sets.newSet();
+        this.playerManager = playerManager;
+        try (DSLContext create = this.getDSL(c)) {
+            Optional.ofNullable(create.selectFrom(table).fetch())
+                    .ifPresent(data -> data.forEach(r -> {
+                        PlayerId id = PlayerId.valueOf(r.getPlyId().intValue());
+                        if (r.getActive()) {
+                            int right = r.getRole().intValue();
+                            String name = r.getName();
+                            playerManager.createPlayer(id, name, PlayerRight.values()[right]);
+                        } else {
+                            this.freeId.add(id);
+                        }
+                    }));
+        }
+    }
+
+    /**
+     * Add a player to the list but will not persist it, persistence is done through external web app. This method is intended to be called AFTER player is persisted to keep data cohesion.
+     *
+     * @param data player to add.
+     */
+    @Override
+    public Player save(final PlayerToCreate data, Connection c) {
+        PlayerId playerId = this.getFreeId(c);
+        try(DSLContext context = this.getDSL(c)) {
+            context.update(table)
+                    .set(table.NAME, data.getLogin())
+                    .set(table.ROLE, (byte)0)
+                    .set(table.ACTIVE, Boolean.TRUE)
+                    .where(table.PLY_ID.equal((short)playerId.value))
+                    .execute();
+            return this.playerManager.createPlayer(playerId, data.getLogin());
+        }
+    }
+
+    /**
+     * @return A free Id for a newly created player.
+     */
+    private PlayerId getFreeId(Connection c) {
+        if (this.freeId.isEmpty()) {
+            return this.createNewLine(c);
+        }
+        PlayerId id = this.freeId.iterator().next();
+        this.freeId.remove(id);
+        return id;
+    }
+
+    private PlayerId createNewLine(Connection c) {
+        try (DSLContext create = this.getDSL(c)) {
+            create.insertInto(table).defaultValues().execute();
+            PlayersRecord player = create.fetchOne(table, table.ACTIVE.equal(false));
+            return PlayerId.valueOf(player.getPlyId().intValue());
+        }
+    }
+
+    @Override
+    public void update(Player data, Connection c) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private DSLContext getDSL(Connection c) {
+        Settings settings = new Settings();
+        settings.setExecuteLogging(false);
+        return DSL.using(c, settings);
+    }
+}
